0a9df2dab38f2e23650bea078e664e7a81f0c74d
Chase <chasehill70@gmail.com>
2026-05-27 00:02:09 -0400

    Version 0.3.0

--- src/dataclassio/__init__.py
+++ src/dataclassio/__init__.py
@@ -1,4 +1,8 @@
+import importlib.metadata
+
 from .config2 import FieldOptions as FieldOptions
 from .io_mixin import IOMixin as IOMixin
 from .types import EFS as EFS
 from .types import ExtraFieldStrategy as ExtraFieldStrategy
+
+__version__ = importlib.metadata.version("hill.dataclassio")
